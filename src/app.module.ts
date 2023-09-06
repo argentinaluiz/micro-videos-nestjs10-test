156ca198cd00f9eafa216c02a9ad4b318dc5864e
Luiz Carlos <argentinaluiz@gmail.com>
2023-09-06 05:10:09 +0000

    organizando módulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from './config/config.module';
-import { DatabaseModule } from './database/database.module';
-import { CategoriesModule } from './categories/categories.module';
-import { SharedModule } from './shared/shared.module';
-import { CastMembersModule } from './cast-members/cast-members.module';
+import { ConfigModule } from './nest-modules/config-module/config.module';
+import { SharedModule } from './nest-modules/shared-module/shared.module';
+import { DatabaseModule } from './nest-modules/database-module/database.module';
+import { CategoriesModule } from './nest-modules/categories-module/categories.module';
+import { CastMembersModule } from './nest-modules/cast-members-module/cast-members.module';
+
 
 @Module({
   imports: [
---
+++ src/nest-modules/categories-module/categories.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { CategoriesController } from './categories.controller';
+import * as CategoryProviders from './categories.providers';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { CategoryModel } from '../../core/category/infra/db/sequelize/category-sequelize';
+@Module({
+  imports: [SequelizeModule.forFeature([CategoryModel])],
+  controllers: [CategoriesController],
+  providers: [
+    ...Object.values(CategoryProviders.REPOSITORIES),
+    ...Object.values(CategoryProviders.USE_CASES),
+  ],
+})
+export class CategoriesModule {}
